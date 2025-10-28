2fbd3d736b6406ff8709e7ac76fa5d8475b8ad6d
manhvd-smartosc <manhvd@smartosc.com>
2025-10-28 10:56:23 +0700

    [Refactor] Refactor hook folder

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,13 +1,4 @@
-export { useDebounce } from './common/useDebounce';
-export { useLocalStorage } from './common/useLocalStorage';
-export {
-  useLoginMutation,
-  useRegisterMutation,
-  useForgotPasswordMutation,
-} from './auth';
-export { useCurrentUser } from './user';
-export {
-  useCreateSupplierMutation,
-  useUpdateSupplierMutation,
-  useGetSupplierById,
-} from './supplier';
+export * from './common';
+export * from './auth';
+export * from './user';
+export * from './supplier';
