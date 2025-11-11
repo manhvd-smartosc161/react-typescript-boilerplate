f8e1c56abb88341f7a164b1822231b58c492ecbe
manhvd-smartosc <manhvd@smartosc.com>
2025-11-11 11:00:50 +0700

    [Feat] Integrate audit log list API

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './common';
 export * from './auth';
+export * from './auditLog';
 export * from './email';
 export * from './user';
 export * from './supplier';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './api';
 export * from './user';
 export * from './auth';
+export * from './auditLog';
 export * from './email';
 export * from './lead';
 export * from './profile';
